d47c599d780e80b0e4c30aa92de0329f1863fd8b
Sarah <42073869+sjproctor@users.noreply.github.com>
2021-08-24 12:32:28 -0700

    Update code-challenges.js

--- code-challenges.js
+++ code-challenges.js
@@ -3,12 +3,13 @@
 // To run file: navigate to the correct directory in terminal, $ node code-challenges.js
 
 // Please read all questions thoroughly
-// All problems can be solved with concepts covered in class or in the syllabus
-// Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
 
 // --------------------1) Create a function that takes in a number and determines if a temperature is below boiling point, at boiling point or above boiling point. Boiling point is 212 degrees Fahrenheit.
-// Use the test variables provided below. Expected output: "35 is below boiling point", "350 is above boiling point", "212 is at boiling point"
+
+// a) Explain your process.
+
+// b) Create the code using the test variables provided below. Expected output: "35 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
 var temp1 = 35
 var temp2 = 350
@@ -17,7 +18,10 @@ var temp3 = 212
 
 
 // --------------------2) Create a function that takes in two arrays of numbers and returns a single array with all the values sorted least to greatest.
-// Use the test variables provided below. Expected output: [-9, -7, 0, 3, 7, 8, 9, 13, 22, 36]
+
+// a) Explain your process.
+
+// b) Create the code using the test variables provided below. Expected output: [-9, -7, 0, 3, 7, 8, 9, 13, 22, 36]
 
 var myNumbers1 = [3, 7, 0, 36, -9]
 var myNumbers2 = [8, -7, 22, 9, 13]
@@ -25,15 +29,21 @@ var myNumbers2 = [8, -7, 22, 9, 13]
 
 
 // --------------------3) Create a function that takes in a string of a single word and returns the string with all letters reversed.
-// Use the test variables provided below. Expected output: "ovarb", "eilrahc"
 
-var myString1 = "bravo"
-var myString2 = "charlie"
+// a) Explain your process.
+
+// b) Create the code using the test variables provided below. Expected output: "ovarb", "eilrahc"
+
+var myString1 = "charlie"
+var myString2 = "delta"
 
 
 
-// --------------------4) Create a function that takes in two numbers and subtracts the smaller number from the larger number.
-// Use the two sets of test variables provided below. Expected output: 15, 12
+// --------------------4) Create a function that takes in two numbers and subtracts the smaller number from the larger number. 
+
+// a) Explain your process.
+
+// b) Create the code using the test variables provided below. Expected output: 15, 12
 
 var numberExample1 = 42
 var numberExample2 = 27
@@ -44,8 +54,11 @@ var numberExample4 = 19
 
 
 // --------------------5) Copy/paste your code from #4. Refactor your code to also accept non-number edge cases. Inform your user if the variable passed into the subtraction function is not a number.
-// Use the two sets of test variables provided below. Expected output: "Your input is not a number", 3
 // Hint: There is an operator in JavaScript called typeof :)
+
+// a) Explain your process.
+
+// b) Create the code using the two sets of test variables provided below. Expected output: "Your input is not a number", 3
 
 var numberExampleRefactor1 = 42
 var numberExampleRefactor2 = "hello"
