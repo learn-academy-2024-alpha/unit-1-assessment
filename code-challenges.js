be86dea3f7a0ce7954ce46cc76545a86111b1982
Sarah <42073869+sjproctor@users.noreply.github.com>
2022-01-13 18:03:43 -0800

    Update code-challenges.js

--- code-challenges.js
+++ code-challenges.js
@@ -9,31 +9,31 @@
 
 // Create the code and test each of the variables provided. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-var temp = 42
+const temp = 42
 // var temp = 350
 // var temp = 212
 
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
 
-var myNumbers1 = [3, 7, 0, 36, -9]
-var myNumbers2 = [8, -7, 42, 9, 13]
+const myNumbers1 = [3, 7, 0, 36, -9]
+const myNumbers2 = [8, -7, 42, 9, 13]
 
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "1202 ohcE"
 
-var currentCohort = "Echo 2021"
+const currentCohort = "Echo 2021"
 
 
 // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
 
-var myArray = [13, 34, 5, 10, 27, 42]
+const myArray = [13, 34, 5, 10, 27, 42]
 
 
 // --------------------5) Create a statement that evaluates two numbers and subtracts the smaller number from the larger number using the two sets of test variables provided below. Expected output: 42, 3
 
-var number1 = 58
-var number2 = 100
+const number1 = 58
+const number2 = 100
 
 // var number1 = 27
 // var number2 = 24
