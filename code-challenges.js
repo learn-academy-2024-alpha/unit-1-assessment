7827401367957e0879749beb9b8dc5e9deaa66cd
Sarah Proctor <sproctor950@gmail.com>
2023-07-17 17:14:54 -0700

    🦊 updates for foxtrot

--- code-challenges.js
+++ code-challenges.js
@@ -1,12 +1,13 @@
 // ASSESSMENT 1: Coding Practical Questions
 
-// To run file: navigate to the correct directory in terminal, $ node code-challenges.js
+// Please read all questions thoroughly
+// Pseudo coding is REQUIRED
+// If you get stuck, leave comments to help us understand your thought process
 
-// Pseudo coding your process is required.
+// Reminder: Ensure you are in the correct directory
+// Run the file with the following command: $ node code - challenges.js
 
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in two strings and determines which of the two strings has more characters. Use the two sets of test variables provided.
-
-// Pseudo code:
 
 // Set one:
 const fruit1 = "apple"
@@ -18,9 +19,9 @@ const fruit3 = "cherry"
 const fruit4 = "kiwi"
 // Expected outcome: "cherry"
 
-// --------------------1) Create a function that takes in a number and determines if the given number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below.
-
 // Pseudo code:
+
+// --------------------1) Create a function that takes in a number and determines if the given number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below.
 
 const temperature1 = 42
 // Expected output: "42 is below boiling point"
@@ -31,24 +32,24 @@ const temperature2 = 350
 const temperature3 = 212
 // Expected output: "212 is at boiling point"
 
-// --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
-
 // Pseudo code:
+
+// --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
 const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
 const padres1998WorldSeriesRuns = [6, 3, 5, 3]
 // Expected output: 9
 
+// Pseudo code:
+
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
 
-// Pseudo code:
+const currentCohort = "Foxtrot 2023"
+// Expected output: "3202 tortxoF"
 
-const currentCohort = "Echo 2023"
-// Expected output: "3202 ohcE"
+// Pseudo code:
 
 // --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
-
-// Pseudo code:
 
 const numberOfConnections = [13, 34, 42, 5, 5, 10, 27, 42, 10]
 
@@ -58,12 +59,14 @@ const givenValue1 = 42
 const givenValue2 = 10
 // Expected output: 8
 
-// --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
-
 // Pseudo code:
+
+// --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
 
 const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
 // Expected output: [82, 80, 79, 77, 76, 73, 72]
 
 const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
 // Expected output: [68, 67, 66, 66, 62, 59, 59]
+
+// Pseudo code:
